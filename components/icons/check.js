6210ec5b2c90c1fede47e97c2d0dd43754a461d0
yalcin oksuz <stakervali@yandex.com>
2021-12-15 20:51:48 +0300

    fixed strokeline typo

--- components/icons/check.js
+++ components/icons/check.js
@@ -3,8 +3,8 @@ import React from 'react';
 function Check(props) {
     return (
         <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
-                                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                       stroke-width="2.5"
+                                  <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
+                                       strokeWidth="2.5"
                                        className="w-3 h-3" viewBox="0 0 24 24">
                                     <path d="M20 6L9 17l-5-5"></path>
                                   </svg>
--- components/icons/rightArrow.js
+++ components/icons/rightArrow.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function RightArrow({style}) {
     return (
-        <svg fill="none" style={style} stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-             stroke-width="2" className="w-4 h-4 ml-2 inline-flex" viewBox="0 0 24 24">
+        <svg fill="none" style={style} stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
+             strokeWidth="2" className="w-4 h-4 ml-2 inline-flex" viewBox="0 0 24 24">
             <path d="M5 12h14M12 5l7 7-7 7"></path>
         </svg>
     );
