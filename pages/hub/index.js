42227d049ff7a4a1360b22cad2ebd5a02a52500d
stakervali <60194710+stakervali@users.noreply.github.com>
2022-03-09 21:00:30 +0300

    Updated hub page with latest github changes.

--- pages/hub/index.js
+++ pages/hub/index.js
@@ -8,9 +8,9 @@ export default function Hub() {
             <h1 className="header-1" >HUB - Tools - Documentation - Channels - Articles</h1>
 
             <p>
-                GRIN scripts and tools. Find scripts in <a className="text-primary"
+                GRIN scripts and tools. You can find scripts in <a className="text-primary"
                                                             href="https://github.com/grincc/tools/tree/main/scripts"
-            >this directory</a>. Other tools are in the linked repositories can be found below
+            >this directory</a>. Other resources of <a href="https://github.com/grincc/hub/tree/main/art" >art</a>, <a href="https://github.com/grincc/hub/tree/main/marketing">marketing</a>, <a href="https://github.com/grincc/hub/tree/main/memes">memes</a> and <a href="https://github.com/grincc/hub/tree/main/wallpapers">wallpapers</a> are also available.
             </p>
 
             {/*Grin Block Explorers*/}
@@ -18,44 +18,44 @@ export default function Hub() {
                 <h2 className="header-2">Hub of links to other repositories with Grin tools and scripts</h2>
 
                 <ul className="pl-4 pt-2 space-y-4" >
-                    <li><RhombusItem />  <b>GrinVention:</b> Pure Python implementation of Mimblewimble protocol for Grin cryptocurrency
+                    <li><RhombusItem />  <b>GrinVention:</b> Pure Python implementation of Mimblewimble protocol for Grin cryptocurrency.
                         <p className="pl-8">
                         <a href="https://github.com/grinventions/" className="text-primary">https://github.com/grinventions/</a>
                         </p>
                     </li>
 
-                    <li><RhombusItem /> <b>Grin Mail:</b> Automatically send and receive transactions via email (last commit 05-07-2021)
+                    <li><RhombusItem /> <b>Grin Mail:</b> Automatically send and receive transactions via email (last commit 05-07-2021).
                         <p className="pl-8">
                             <a href="https://github.com/aixinrock/grinmail/blob/main/README-en.md" className="text-primary">https://github.com/aixinrock/grinmail/blob/main/README-en.md</a>
                         </p>
                     </li>
 
 
-                    <li><RhombusItem /> <b>Testnet exchange: </b> Template solution for exchanges consits of 2 pieces 1) the Grin Wallet APIs for Grin community wallet developers via a JSON-RPC interface 2) areference command-line wallet, which provides a baseline wallet for Grin and demonstrates how the wallet APIs should be called.
+                    <li><RhombusItem /> <b>Testnet exchange: </b> An example implementation for testnet exchange that consists of 2 pieces 1) the Grin Wallet APIs for Grin community wallet developers via a JSON-RPC interface 2) a reference command-line wallet, which provides a baseline wallet for Grin and demonstrates how the wallet APIs should be called.
                         <p className="pl-8">
                             <a href="https://github.com/pkariz/grin-wallet/tree/fix/invoice-issues" className="text-primary">https://github.com/pkariz/grin-wallet/tree/fix/invoice-issues </a>
                         </p>
                     </li>
 
-                    <li><RhombusItem /> <b> grin-wallet: </b>command line (Rust), backend for Niffler and Ironbelly wallet
+                    <li><RhombusItem /> <b> grin-wallet: </b>Command line (Rust), backend for Niffler and Ironbelly wallet.
                         <p className="pl-8">
                             <a href="https://github.com/mimblewimble/grin-wallet" className="text-primary"> https://github.com/mimblewimble/grin-wallet </a>
                         </p>
                     </li>
 
-                    <li><RhombusItem /> <b> Grin++: </b>  wallet (written in C++), supports desktop and mobile
+                    <li><RhombusItem /> <b> Grin++: </b>  Wallet (written in C++), supports desktop and mobile.
                         <p className="pl-8">
                             <a href="https://grinplusplus.github.io/" className="text-primary">https://grinplusplus.github.io/</a>
                         </p>
                     </li>
 
-                    <li><RhombusItem /> <b>Ironbelly: </b> mobile Grin wallet (Android and iOS), runs a lite node optionally connects to your own node for extra privacy
+                    <li><RhombusItem /> <b>Ironbelly: </b> Mobile Grin wallet (Android and iOS), runs a lite node optionally connects to your own node for extra privacy.
                         <p className="pl-8">
                             <a href=" " className="text-primary">https://github.com/i1skn/ironbelly</a>
                         </p>
                     </li>
 
-                    <li><RhombusItem /> <b> Niffler GUI wallet:  </b> Rust grin-wallet (rust) as backend
+                    <li><RhombusItem /> <b> Niffler GUI wallet:  </b> Rust grin-wallet (rust) as backend.
                         <p className="pl-8">
                             <a href=" " className="text-primary"> https://github.com/grinfans/niffler </a>
                         </p>
@@ -81,9 +81,9 @@ export default function Hub() {
 
             {/*Other link outs*/}
             <div className="py-4">
-                <h2 className="header-2">Other link outss</h2>
+                <h2 className="header-2">Other link outs</h2>
                 <ul className="pl-4 pt-2 space-y-4" >
-                    <li><RhombusItem />  <b>Grin official website: </b> Find all information and links to node and wallet software you might be searching for
+                    <li><RhombusItem />  <b>Grin official website: </b> Find all information and links to node and wallet software you might be searching for.
                         <p className="pl-8">
                             <a href="https://grin.mw/" className="text-primary">https://grin.mw/</a>
                         </p>
@@ -95,7 +95,7 @@ export default function Hub() {
                         </p>
                     </li>
 
-                     <li><RhombusItem />  <b>Grinnode.live:  </b> High-available GRIN-API & Sync-Service (very convenient if you are a developer)
+                     <li><RhombusItem />  <b>Grinnode.live:  </b> High-available GRIN-API & Sync-Service (very convenient if you are a developer).
                          <p className="pl-8">
                             <a href="https://grinnode.live/" className="text-primary">https://grinnode.live/</a>
                         </p>
@@ -107,25 +107,37 @@ export default function Hub() {
                         </p>
                     </li>
 
-                     <li><RhombusItem />  <b>Grin documentation: </b> Your journey down the rabit hole starts here! Learn about where Grin comes from, Mimble Wimble, Pederson commitments, Eliptic Curve cryptography as well as practical tutorails on how to install and use the grin node software and grin-wallet.
+                     <li><RhombusItem />  <b>Grin documentation: </b> Your journey down the rabbit hole starts here! Learn about where Grin comes from, Mimblewimble, Pedersen commitments, Elliptic Curve cryptography as well as practical tutorials on how to install and use the grin node software and grin-wallet.
                          <p className="pl-8">
                             <a href="https://docs.grin.mw/" className="text-primary">https://docs.grin.mw/</a>
                         </p>
                     </li>
 
-                     <li><RhombusItem />  <b> Grin link to old mailing list</b>
+                     <li><RhombusItem />  <b> Grinflation</b>
+                        <p className="pl-8">
+                            A great website that shows all the economics around Grins's Fair 1   ツ per second supply rate, its inflation, total supply, market cap and price now and in times to come.
+                            <a href="https://grinflation.com/" className="text-primary">https://grinflation.com/</a>
+                        </p>
+                    </li>
+
+                     <li><RhombusItem />  <b>A Different approach to Grin's inflation supply: </b> A nice article showing a different view on Grin's supply, inflation and inherent fairness.
+                         <p className="pl-8">
+                            <a href="https://madenuzmani.medium.com/a-different-approach-to-grins-inflation-and-supply-355266fc96be" className="text-primary">https://madenuzmani.medium.com/a-different-approach-to-grins-inflation-and-supply-355266fc96be</a>
+                        </p>
+                    </li>
+                    <li><RhombusItem />  <b>Grin link to old mailing list: </b>
                         <p className="pl-8">
                             <a href="https://lists.launchpad.net/mimblewimble/" className="text-primary">https://lists.launchpad.net/mimblewimble/</a>
                         </p>
                     </li>
-
-                     <li><RhombusItem />  <b>Grin Papyrus: </b> Collection of important documents and links related to the development of GRIN. Find back everything, the early steps towards Grin in 2016, launch in 2019 and everything that happened after
-                         <p className="pl-8">
+                    <li><RhombusItem />  <b>Grin Papyrus: </b> Collection of important documents and links related to the development of GRIN. Find back everything, the early steps towards Grin in 2016, launch in 2019 and everything that happened after
+                        <p className="pl-8">
                             <a href="https://github.com/Grinnode-live/GRIN-papyrus" className="text-primary">https://github.com/Grinnode-live/GRIN-papyrus</a>
                         </p>
                     </li>
 
-                     <li><RhombusItem />  <b>What is Grin?</b> Explenation on emission, transaction aggretation and mimble wimble with some nice visuals usefull for presentations, by Phyro.
+
+                     <li><RhombusItem />  <b>What is Grin?</b> Explanation on emission, transaction aggretation and Mimblewimble with some nice visuals usefull for presentations, by Phyro.
                          <p className="pl-8">
                             <a href="https://github.com/phyro/what-is-grin" className="text-primary">https://github.com/phyro/what-is-grin</a>
                         </p>
@@ -145,7 +157,7 @@ export default function Hub() {
                 <h2 className="header-2">External articles</h2>
 
                 <ul className="pl-4 pt-2 space-y-4" >
-                    <li><RhombusItem />  <b>Articles on Grin and Mimble Wimble: </b> Just a few of the many, feel free to add more.
+                    <li><RhombusItem />  <b>Articles on Grin and Mimblewimble: </b> Just a few of the many, feel free to add more.
                         <p className="pl-8">
                             <a href="https://www.blockchainbeach.com/going-deep-on-privacy-mimblewimble/" className="text-primary">https://www.blockchainbeach.com/going-deep-on-privacy-mimblewimble/</a>
                         </p>
@@ -163,7 +175,7 @@ export default function Hub() {
                 <h2 className="header-2">Community channels and social media</h2>
 
                 <ul className="pl-4 pt-2 space-y-4" >
-                    <li><RhombusItem />  <b>KeyBase:</b> Grincoin on keybase is the best place for technical discussions and to join general meetings
+                    <li><RhombusItem />  <b>KeyBase:</b> Grincoin on keybase is the best place for technical discussions and to join general meetings.
                         <p className="pl-8">
                             <a href="https://keybase.io/team/grincoin" className="text-primary">https://keybase.io/team/grincoin</a>
 
@@ -176,7 +188,7 @@ export default function Hub() {
                         </p>
                     </li>
 
-                    <li><RhombusItem />  <b>Grin Telegram community:</b> Buzzing with discussions memes, and anything else Grin related
+                    <li><RhombusItem />  <b>Grin Telegram community:</b> Buzzing with discussions memes, and anything else Grin related.
                         <p className="pl-8">
                             <a href="https://t.me/grinprivacy" className="text-primary">https://t.me/grinprivacy</a>
                         </p>
@@ -194,7 +206,7 @@ export default function Hub() {
                         </p>
                     </li>
 
-                    <li><RhombusItem />  <b>Grinventions Telegram channel:</b> Innovative inventions for the grin cryptocurrency. Also discusses building the MimbleWimble Python implementatoin and quantum computing basic in Python
+                    <li><RhombusItem />  <b>Grinventions Telegram channel:</b> Innovative inventions for the grin cryptocurrency. Also discusses building the Mimblewimble Python implementatoin and quantum computing basic in Python
                         <p className="pl-8">
                             <a href="https://t.me/grinventions" className="text-primary">https://t.me/grinventions</a>
 
@@ -204,13 +216,22 @@ export default function Hub() {
                     <li><RhombusItem />  <b>Grin Twitter:</b> Activities suspender bla bla. When they try to silence you ... you know you are on the right path!
                         <p className="pl-8">
                             <a href="https://twitter.com/grinMW" className="text-primary">https://twitter.com/grinMW</a>
-
                         </p>
                     </li>
 
-                    <li><RhombusItem />  <b>Grin Reddit</b> Not that active, for more busy channels, check Telegram, Keybase and the forum
+                    <li><RhombusItem />  <b>Grin Reddit</b> Not that active, for more busy channels, check Telegram, Keybase and the forum.
                         <p className="pl-8">
                             <a href="https://www.reddit.com/r/GRIN/" className="text-primary">https://www.reddit.com/r/GRIN/</a>
+                        </p>
+                    </li>
+                    <li><RhombusItem />  <b>Grin Reddit (grincoin)</b> Another reddit channel which is gaining traction.
+                        <p className="pl-8">
+                            <a href="https://www.reddit.com/r/grincoin/" className="text-primary">https://www.reddit.com/r/grincoin/</a>
+                        </p>
+                    </li>
+                    <li><RhombusItem />  <b>Grin Discord community</b> Meet other grin followers on discord.
+                        <p className="pl-8">
+                            <a href="https://discord.gg/5p4vCQY9km" className="text-primary">https://discord.gg/5p4vCQY9km</a>
                         </p>
                     </li>
                 </ul>
